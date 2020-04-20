ac2b5a425a10b4bb7a8ecbde1275720fb0c886d5
Parvis H <phaydark@gmail.com>
2020-04-19 22:25:29 -0600

    fixed the logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -19,28 +19,32 @@ let cards = [
 let cardsInPlay = [];
 
 function checkForMatch(){
-    if (cardsInPlay.length === 2){
+    //if (cardsInPlay.length === 2){
       if (cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match!");
       }else {
         alert("Sorry, try again.");
       }
-}}
+};
 function flipCard(){
     let cardId = this.getAttribute('data-id');
+    cardsInPlay.push(cards[cardId].rank);
     this.setAttribute("src", cards[cardId].cardImage);
-    cardsInPlay.push(cards[cardId].cardImage);
+    //cardsInPlay.push(cards[cardId].cardImage);
     //console.log(cards[cardId].cardImage);
-    cardsInPlay.push(cards[cardId].suit);
+    
     //console.log(cards[cardId].suit)
-    checkForMatch();
+    if (cardsInPlay.length === 2){
+        checkForMatch();
+    }
+    
 }
 function createBoard(){
 for (let i = 0; i < cards.length; i++) {
 
   let cardElement = document.createElement('img');
       cardElement.setAttribute("src", "images/back.png");
-      cardElement.setAttribute('data-id', [i]);
+      cardElement.setAttribute('data-id', i);
       cardElement.addEventListener("click", flipCard);
       document.getElementById('game-board').appendChild(cardElement);
 }
